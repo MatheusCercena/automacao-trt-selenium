1baacaa2e03a7394a9256f7131ccdf53fab4bc46
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-06-12 13:31:25 -0300

    .

--- script.py
+++ script.py
@@ -1,10 +1,16 @@
-from busca_dados import criar_dados
+# from busca_dados import criar_dados
+from utils import validar_ordem_de_servico
 
+lista_de_pedidos = []
 
 while True:
-    ordem_de_servico = input('Digite a ordem de srviço no formato xxxx/xx-x: ')
-    break
+    
+    ordem_de_servico = input('Digite a ordem de serviço no formato xxxx/xx-x: ').strip()
+    while validar_ordem_de_servico(ordem_de_servico) != True:
+        print('A ordem de serviço informada não é valida.')
+        ordem_de_servico = input('Digite a ordem de serviço no formato xxxx/xx-x: ').strip()
+    lista_de_pedidos.append(ordem_de_servico)
+    print(f'OS {ordem_de_servico} adicionada com sucesso. Até o momento, foram adicionadas as OS {', '.join(lista_de_pedidos)}.')
+    res = input('Digite a ordem de serviço da próxima TRT ou para')
+# criar_dados(ordem_de_servico)
 
-criar_dados(ordem_de_servico)
-
-#for
---
+++ utils.py
@@ -0,0 +1,13 @@
+def validar_ordem_de_servico(ordem_de_servico):
+    try:
+        partes1_e_2, parte3 = ordem_de_servico.split('-')
+        parte1, parte2 = partes1_e_2.split('/')
+        parte1 = int(parte1)
+        parte2 = int(parte2)
+        parte3 = int(parte3)
+        return True
+    except:
+        return False
+    
+t = input('Teste: ')
+print(validar_ordem_de_servico(t))
