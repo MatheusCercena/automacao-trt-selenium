429e194d704cf43de44d37a57c3803fa5d6a3e49
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-07-14 15:04:33 -0300

    adicionando funcao pra dar baixa nas trts

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from pipelines import *
 from inputs import solicitar_ordens_de_servico
+from dar_baixa import dar_baixa
 
 if __name__ == "__main__":
-    lista_de_pedidos = solicitar_ordens_de_servico()
+    res = ''
+    valores_res = ['1', '2']
+    while res not in valores_res:
+        res = input('''
+O que deseja fazer? 
+[1] Cadastrar novas TRTs.
+[2] Dar baixa nas TRTs
+Opção: ''')
     navegador = abrir_navegador()
-    fazer_logins(navegador)
-    for ordem_de_servico in lista_de_pedidos:
-        dados = criar_dados(navegador, ordem_de_servico)
-        print(f' Dados da OS ({ordem_de_servico}): {dados}')
-        preencher_trts(navegador, dados)
+    if res == '1': 
+        lista_de_pedidos = solicitar_ordens_de_servico()
+        fazer_logins(navegador)
+        for ordem_de_servico in lista_de_pedidos:
+            dados = criar_dados(navegador, ordem_de_servico)
+            print(f' Dados da OS ({ordem_de_servico}): {dados}')
+            preencher_trts(navegador, dados)
+    elif res == '2': 
+        fazer_logins(navegador)
+        dar_baixa(navegador)
 
--- preenchimento_trt.py
+++ preenchimento_trt.py
@@ -106,49 +106,49 @@ def validacao(navegador):
     else:
         return True
         
-def emitir_boleto(navegador, dados):
-    esperar = WebDriverWait(navegador, 20)
-    esperar.until(lambda navegador: 'https://servicos.sinceti.net.br/app/view/sight/ini?form=Art&id=' in navegador.current_url)
-    try:
-        clicar(navegador, By.ID, 'emitirBoleto')
-    except:
-        pass
-    try:
-        clicar(navegador, By.ID, 'save')
-    except:
-        pass
-    esperar_ajax(navegador)
-    espera_objeto = WebDriverWait(navegador, 10)
-    espera_objeto.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'html body div.site div#conteudo_container div#conteudo div.cad_conteudo div.cad_conteudo_sub div#cad_botoes a.botao_imprimir')))
-    botao = navegador.find_element(By.CSS_SELECTOR, 'a.botao_imprimir:nth-child(6)')
-    onclick = botao.get_attribute("onclick")
+# def emitir_boleto(navegador, dados):
+#     esperar = WebDriverWait(navegador, 20)
+#     esperar.until(lambda navegador: 'https://servicos.sinceti.net.br/app/view/sight/ini?form=Art&id=' in navegador.current_url)
+#     try:
+#         clicar(navegador, By.ID, 'emitirBoleto')
+#     except:
+#         pass
+#     try:
+#         clicar(navegador, By.ID, 'save')
+#     except:
+#         pass
+#     esperar_ajax(navegador)
+#     espera_objeto = WebDriverWait(navegador, 10)
+#     espera_objeto.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'html body div.site div#conteudo_container div#conteudo div.cad_conteudo div.cad_conteudo_sub div#cad_botoes a.botao_imprimir')))
+#     botao = navegador.find_element(By.CSS_SELECTOR, 'a.botao_imprimir:nth-child(6)')
+#     onclick = botao.get_attribute("onclick")
 
-    match = re.search(r"window\.open\('([^']+)'", onclick)
-    url_boleto = match.group(1)
+#     match = re.search(r"window\.open\('([^']+)'", onclick)
+#     url_boleto = match.group(1)
 
-    nome_saida = f"Boleto_TRT_{dados['nome']}_{dados['numero_orcamento'].replace('/', '-')}.pdf"
-    arquivo_saida = os.path.join('TRTs', nome_saida)
-    headers = {
-        "User-Agent": "Mozilla/5.0",
-    }
-    sessao = requests.Session()
-    sessao.headers.update(headers)
-    sessao.cookies.update({cookie['name']: cookie['value'] for cookie in navegador.get_cookies()})
+#     nome_saida = f"Boleto_TRT_{dados['nome']}_{dados['numero_orcamento'].replace('/', '-')}.pdf"
+#     arquivo_saida = os.path.join('TRTs', nome_saida)
+#     headers = {
+#         "User-Agent": "Mozilla/5.0",
+#     }
+#     sessao = requests.Session()
+#     sessao.headers.update(headers)
+#     sessao.cookies.update({cookie['name']: cookie['value'] for cookie in navegador.get_cookies()})
 
-    resposta = sessao.get(url_boleto)
-    soup = BeautifulSoup(resposta.text, "html.parser")
+#     resposta = sessao.get(url_boleto)
+#     soup = BeautifulSoup(resposta.text, "html.parser")
 
-    iframe = soup.find("iframe")
-    if iframe and 'src' in iframe.attrs:
-        url_pdf_real = iframe['src']
-    else:
-        raise Exception("PDF não encontrado no iframe.")
+#     iframe = soup.find("iframe")
+#     if iframe and 'src' in iframe.attrs:
+#         url_pdf_real = iframe['src']
+#     else:
+#         raise Exception("PDF não encontrado no iframe.")
 
-    resposta_pdf = sessao.get(url_pdf_real)
-    with open(arquivo_saida, "wb") as f:
-        f.write(resposta_pdf.content)
-    res = input("O boleto foi impresso? Digite 'parar' para parar.")
-    if res == 'parar':
-        return False
-    else:
-        return True
+#     resposta_pdf = sessao.get(url_pdf_real)
+#     with open(arquivo_saida, "wb") as f:
+#         f.write(resposta_pdf.content)
+#     res = input("O boleto foi impresso? Digite 'parar' para parar.")
+#     if res == 'parar':
+#         return False
+#     else:
+#         return True
