46bd3e6740f315634eeb258f5b97b018ecaeb33f
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-08-21 20:08:19 -0300

    ...

---
+++ config.example.py
@@ -0,0 +1,7 @@
+# Arquivo de configuração de exemplo
+# Copie este arquivo para config.py e preencha com suas credenciais reais
+
+usuario_ecg = 'seu_usuario_ecg'
+senha_ecg = 'sua_senha_ecg'
+usuario_sinceti = 'seu_usuario_sinceti'
+senha_sinceti = 'sua_senha_sinceti'
