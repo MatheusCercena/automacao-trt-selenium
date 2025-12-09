2516704cfb949d476814267640f9238670e65739
Matheus Cercena do Nascimento <certheus@protonmail.com>
2025-12-09 15:29:04 -0300

    adidionando formatação
    adicionando função para printar dados das trts

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas
 from dar_baixa import dar_baixa
 
 if __name__ == "__main__":
+
     res = ''
     valores_res = ['1', '2']
     while res not in valores_res:
@@ -28,4 +29,59 @@ Opção: ''')
         fazer_logins(navegador)
         dar_baixa(navegador)
 
-# Adicionar uma logica pra verificar se os ultimos numeros do cep sao 000 e entao adicionar o resto do cep manualmente
+#     navegador = abrir_navegador()
+#     lista_de_pedidos = ["1933/25-1", "1984/25-1", "1982/25-1", "1980/25-1", "2000/25-1", "2024/25-1", "1983/25-1", "2044/25-1", "1990/25-1", "1985/25-1", "2018/25-1", "1998/25-1", "2125/25-1", "2020/25-1", "1997/25-1"]
+#     fazer_logins(navegador)
+
+#     for i, ordem_de_servico in enumerate(lista_de_pedidos):
+#         dados = criar_dados(navegador, ordem_de_servico)
+#         # Formatar CPF
+#         cpf_formatado = f"{dados['cpf'][:3]}.{dados['cpf'][3:6]}.{dados['cpf'][6:9]}-{dados['cpf'][9:]}"
+#         # Formatar Preço (assumindo que 567000 é R$ 567.000,00)
+#         preco_com_virgula = dados['preco'][:-2] + "," + dados['preco'][-2:]
+#         preco_formatado = f"R$ {float(preco_com_virgula.replace(',', '.')):,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+#         # Formatar Datas (assumindo formato DDMMYYYY)
+#         data_inicial_formatada = f"{dados['data_inicial'][:2]}/{dados['data_inicial'][2:4]}/{dados['data_inicial'][4:]}"
+#         data_final_formatada = f"{dados['data_final'][:2]}/{dados['data_final'][2:4]}/{dados['data_final'][4:]}"
+#         area_string = str(dados['area'])
+#         area_formatada = area_string[:-2] + "," + area_string[-2:] # Resultado: '3,65'
+#         # Geração do output formatado
+#         output_formatado = f"""
+# ==================================================
+#            DADOS DO ORÇAMENTO N° {dados['numero_orcamento']}
+# ==================================================
+
+# INFORMAÇÕES DO CLIENTE
+
+# Nome Completo: {dados['nome']}
+# Nome Fantasia: {dados['fantasia']}
+# CPF: {cpf_formatado}
+# Gênero: {dados['genero']}
+# E-mail: {dados['email']}
+# Telefone: {dados['telefone']}
+
+# ENDEREÇO
+
+# Logradouro: {dados['logradouro']}, {dados['numero']}
+# Complemento: {dados['complemento']}
+# CEP: {dados['cep']}
+# Cidade/UF: {dados['cidade']}/{dados['uf']}
+
+# DETALHES DO PROJETO
+
+# Área (m²): {area_formatada}
+# Cor da Esquadria: {dados['cor'].capitalize()}
+# Especificação do Vidro: {dados['vidro'].capitalize()}
+
+# VALOR E PRAZOS
+
+# Preço Total: {preco_formatado}
+# Data Início Prevista: {data_inicial_formatada}
+# Data Fim Prevista: {data_final_formatada}
+
+# OBSERVAÇÕES
+
+# {dados['observacao']}
+# """
+
+#         print(output_formatado)
